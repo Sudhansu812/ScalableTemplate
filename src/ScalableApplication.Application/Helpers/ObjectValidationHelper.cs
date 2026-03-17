3ca62a5e14ddca6480b2d27a86768ff32ec0cd18
Sudhansu Kumar Maharana <sudhansumaharana812@gmail.com>
2026-03-17 15:35:57 +0530

    Add object validation helper and update NuGet packages
    Introduced ObjectValidationHelper and IObjectValidationHelper for reusable, interface-driven model validation using data annotations. Upgraded multiple NuGet dependencies to latest versions and added Microsoft.AspNetCore.Identity.EntityFrameworkCore. Also removed an unused folder reference from the application project.

---
+++ src/ScalableApplication.Application/Helpers/ObjectValidationHelper.cs
@@ -0,0 +1,40 @@
+﻿using ScalableApplication.Application.Interfaces.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ScalableApplication.Application.Helpers
+{
+    public class ObjectValidationResult
+    {
+        public bool IsValid { get; set; }
+        public string? Error { get; set; }
+    }
+
+    public class ObjectValidationHelper : IObjectValidationHelper
+    {
+        public ObjectValidationResult ValidateObject<T>(T obj)
+        {
+            List<ValidationResult> validationResults = new();
+            StringBuilder errors = new();
+
+            if (!Validator.TryValidateObject(obj!, new ValidationContext(obj!, null, null), validationResults, true))
+            {
+                foreach (var err in validationResults)
+                {
+                    errors.AppendLine($"{err.ErrorMessage}");
+                }
+                return new ObjectValidationResult
+                {
+                    IsValid = false,
+                    Error = errors.ToString()
+                };
+            }
+
+            return new ObjectValidationResult
+            {
+                IsValid = true,
+                Error = string.Empty
+            };
+        }
+    }
+}
---
+++ src/ScalableApplication.Application/Interfaces/Helpers/IObjectValidationHelper.cs
@@ -0,0 +1,9 @@
+﻿using ScalableApplication.Application.Helpers;
+
+namespace ScalableApplication.Application.Interfaces.Helpers
+{
+    public interface IObjectValidationHelper
+    {
+        public ObjectValidationResult ValidateObject<T>(T obj);
+    }
+}
